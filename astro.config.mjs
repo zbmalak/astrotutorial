c35d4e9d4ca3067584feeb15ae15754e2c3a9788
zbmalak <zbigniew.malak@gmail.com>
2024-01-05 20:20:16 +0100

    finished

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://fancy-beijinho-7f3b98.netlify.app',
+  integrations: [preact()]
+});
